66f441baf8ed6d62ef09db91e350427f988500d1
Wolfgang Koenig <wko@mms-t-systems.de>
2020-11-27 15:46:29 +0100

    26.11.20

--- java/data/src/main/java/de/wko/mdb/data/repository/AlbumCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package de.wko.mdb.data.repository;
-
-import de.wko.mdb.data.entity.AlbumEntity;
-
-import java.util.List;
-
-public interface AlbumCustomRepository {
-    public List<AlbumEntity> findByArtistId(Long artistId);
-}
